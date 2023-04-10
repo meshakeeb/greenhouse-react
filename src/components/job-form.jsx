c91febd15f77eb5b4d0e554c8d67e86c064587f5
Shakeeb Ahmed <me@shakeebahmed.com>
2023-04-11 00:30:48 +0300

    Final error changes

--- src/components/job-form.jsx
+++ src/components/job-form.jsx
@@ -104,10 +104,13 @@ const onSubmit = async (data, classTitle, reset, setSuccess, setSubmitting, setE
 					email: data.email
 				})
 			}
+		} else {
+			setSubmitting(false)
+			setError('Something went wrong! The team has been notified. Please submit again.')
 		}
 	}).catch((error) => {
 		setSubmitting(false)
-		setError(error.message)
+		setError('Something went wrong! The team has been notified. Please submit again.')
 	})
 }
 
@@ -185,6 +188,7 @@ export default function JobForm({ jobId, classTitle, questions }) {
 			<h4 className="font-semibold mb-6">Apply for this Class</h4>
 			<form className="job-form" onSubmit={handleSubmit( async (data)=> {
 				setSubmitting(true)
+				setError(false)
 				await onSubmit(data, classTitle, reset, setSuccess, setSubmitting, setError)
 			})}>
 
@@ -192,17 +196,19 @@ export default function JobForm({ jobId, classTitle, questions }) {
 
 				{questions.map((question, index) => <FormField register={register} key={`question-${index}`} errors={errors} question={question} />)}
 
-				{error && <p>{error}</p>}
+				{error && <div className="mb-4"><p>{error}</p></div>}
 
-				{isSubmitting
-					? (
-						<button disabled={true}>
-							<svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
-							Submitting&hellip;
-						</button>
-					)
-					: (<button type="submit">Submit</button>)
-				}
+				<div>
+					{isSubmitting
+						? (
+							<button disabled={true}>
+								<svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+								Submitting&hellip;
+							</button>
+						)
+						: (<button type="submit">Submit</button>)
+					}
+				</div>
 
 			</form>
 		</div>
