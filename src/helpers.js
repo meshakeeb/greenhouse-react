9d22840f418101f5598836555ec657df2fc409a3
Shakeeb Ahmed <me@shakeebahmed.com>
2023-08-29 00:59:44 +0300

    New fixes

--- src/helpers.js
+++ src/helpers.js
@@ -19,8 +19,8 @@ export function normalizeTitle( rawTitle ) {
 	const title = parts[0].split(' - ')
 	const dates = parts[1].split(' - ')
 
-	const d1 = new Date(dates[0])
-	const d2 = new Date(dates[1])
+	const d1 = new Date(dates[0].substr(0,10))
+	const d2 = new Date(dates[1].substr(0,10))
 	const options = { month:"long", day:"numeric", year: "numeric"}
 	const formatter = new Intl.DateTimeFormat('en-US', options)
 
